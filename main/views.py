5cfd34cd08e2401e585c2997e2e0034d931dca81
yoons02 <kys5037k@naver.com>
2023-01-17 04:27:01 +0900

    페이징 처리, css

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Blog
 from django.utils import timezone
+from django.core.paginator import Paginator
 
 # Create your views here.
 def showmain(request):
@@ -23,7 +24,11 @@ def showannounce(request):
 
 def showmessage(request):
     blogs = Blog.objects.all()
-    return render(request, 'main/message.html',{'blogs':blogs})
+
+    p = Paginator(Blog.objects.order_by('-id'),10)
+    page = request.GET.get('page')
+    venues = p.get_page(page)
+    return render(request, 'main/message.html',{'blogs':blogs, 'venues':venues})
 
 def showdreamer(request):
     return render(request, 'main/dreamer.html')
@@ -61,3 +66,20 @@ def delete(request, id):
     delete_blog = Blog.objects.get(id = id)
     delete_blog.delete()
     return redirect('main:showmessage')
+
+# def test(request):
+#     blog_list = Blog.objects.all()
+#     page = request.Get.get('page', '1')
+#     paginator = Paginator(blog_list,'10')
+#     page_obj = paginator.page(page)
+#     return render(request, 'main:showmessage', {'page_obj':page_obj})
+
+# def showmessage(request):
+#     blog_list = Blog.objects.all() #models.py Board 클래스의 모든 객체를 board_list에 담음
+#     # board_list 페이징 처리
+#     page = request.GET.get('page', '1') #GET 방식으로 정보를 받아오는 데이터
+#     paginator = Paginator(blog_list, '10') #Paginator(분할될 객체, 페이지 당 담길 객체수)
+#     page_obj = paginator.page(page) #페이지 번호를 받아 해당 페이지를 리턴 get_page 권장
+#     return render(request, 'main:message.html', {'page_obj':page_obj}) 
+
+
